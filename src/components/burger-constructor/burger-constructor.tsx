3cb379fca678acc993bd106ab1f4f1d0c9c5e8d5
Anna Kochetkova <kohetkova1999@gmail.com>
2022-03-31 17:38:50 +0300

    sprint 2/ step 1

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,56 +1,78 @@
+import { useContext } from 'react';
+import { ContextIngredients, IngredientsAPI } from '../app/contextIngredients';
 import styles from './burger-constructor.module.css';
 import { ConstructorElement, Button, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import ConstructorIngredients from './constructor-ingredients';
+import { useState } from 'react';
 
 interface IBurgerConstructorProps {
     onOpen: ()=>void;
-    list: any[];
+    //list: any[];
 }
 
 const BurgerConstructor = (props: IBurgerConstructorProps) => {
 
+    const {ingredientsConstructor, amount, bun} = useContext(ContextIngredients);
+    const {createOrder} = useContext(IngredientsAPI);
+    
     const {onOpen} = props;
+
+    const handleCreateOrder = async() => {
+        await createOrder();
+        onOpen();
+    }
 
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
+
                 <div className={`ml-8 ${styles.bun}`}>
-                    <ConstructorElement
-                        type="top"
-                        isLocked={true}
-                        text="Краторная булка N-200i (верх)"
-                        price={200}
-                        thumbnail='https://code.s3.yandex.net/react/code/bun-02.png'
-                    />
-                </div>
-                <div className={styles.ingredients}>
                     {
-                        props.list.map(el => 
-                        {
-                            if(el.type !== "bun" && el.type !== "sauce"){
-                                return <ConstructorIngredients key={el._id} name={el.name} image={el.image} price={el.price}/>
-                            }
-                            
-                        })
-                        
+                        bun !== undefined && (
+                            <ConstructorElement
+                                type="top"
+                                isLocked={true}
+                                text={`${bun.name} (верх)`}
+                                price={bun.price}
+                                thumbnail={bun.image}
+                            /> 
+                        )
                     }
+                   
                 </div>
+
+                <div className={styles.ingredients}>
+                {
+                    ingredientsConstructor.map(el => {
+                        return (
+                            <ConstructorIngredients key={el._id} name={el.name} image={el.image} price={el.price}/>
+                        )
+                    })
+                }
+                </div>
+
+
                 <div className={`ml-8 ${styles.bun}`}>
-                    <ConstructorElement
-                        type="bottom"
-                        isLocked={true}
-                        text="Краторная булка N-200i (низ)"
-                        price={200}
-                        thumbnail='https://code.s3.yandex.net/react/code/bun-02.png'
-                    />
+                    {
+                        bun !== undefined && (
+                            <ConstructorElement
+                                type="bottom"
+                                isLocked={true}
+                                text={`${bun.name} (низ)`}
+                                price={bun.price}
+                                thumbnail={bun.image}
+                            />
+                        )
+                    }
+
                 </div>
             </div>
             <div className={`mt-10 ${styles.orderWrapper}`}>
                 <span className={`text text_type_digits-medium mr-10 ${styles.span}`}>
-                    610
+                    {amount}
                     <CurrencyIcon type="primary" />
                 </span>
-                <Button type="primary" size="large" onClick={onOpen} >
+                <Button type="primary" size="large" onClick={handleCreateOrder} >
                     Оформить заказ
                 </Button>
             </div>
