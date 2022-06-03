078698784b65bb88c170473687afae0d4f9fae8c
Anna Kochetkova <kohetkova1999@gmail.com>
2022-06-03 18:32:12 +0300

    sprint5-appheader

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,11 +1,11 @@
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './app-header.module.css';
 import { Link, NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { RootState } from '../../services/logic/rootReducer';
+import { useSelector } from '../../services/logic/store';
 
 const AppHeader = () => {
-    const user = useSelector((store: RootState) => store.authorization.user);
+    const user = useSelector(store => store.authorization.user);
     return (
         <header className={`p-4 ${styles.header}`}>
             <div className={styles.container}>
